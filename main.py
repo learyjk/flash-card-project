e25903ee75d04a231642493e2167908e641f786c
keeganleary <leary.keegan@gmail.com>
2020-11-11 11:49:08 -0800

    flip_card and next_card

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import json
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
 
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+# --------------- UI --------------- #
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=20, pady=20, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+card_back_img = ImageTk.PhotoImage(Image.open("images/card_back.gif"))
 card_front_img = ImageTk.PhotoImage(Image.open("images/card_front.gif"))
-canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 300, text="trouve", font=("Ariel", 60, "bold"))
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 300, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_img = ImageTk.PhotoImage(Image.open("images/wrong.gif"))
-wrong_button = Button(image=wrong_img, highlightthickness=0)
-wrong_button.grid(column=0, row=1)
+unknown_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
+unknown_button.grid(column=0, row=1)
 
 correct_img = ImageTk.PhotoImage(Image.open("images/right.gif"))
-correct_button = Button(image=correct_img, highlightthickness=0)
-correct_button.grid(column=1, row=1)
+known_button = Button(image=correct_img, highlightthickness=0, command=next_card)
+known_button.grid(column=1, row=1)
+
+next_card()
 
 window.mainloop()
